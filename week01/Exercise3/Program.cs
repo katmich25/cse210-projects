1b46e4f0e056457b27dd19955f4646e0d5f51938
katmich25 <mich_elin@live.com.mx>
2026-09-05 16:14:41 -0600

    Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,33 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        Console.Write("What is the magic number? ");
-        string answer = Console.ReadLine();
-        int magicN = int.Parse(answer);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
 
+    
         int guess = 0;
 
-        while (guess == magicN)
+        while (guess != number)
         {
             Console.Write("What is your guess? ");
             string answerGuess = Console.ReadLine();
-            int guess = int.Parse(answerGuess);
+            guess = int.Parse(answerGuess);
             
-            if (guess > magicN)
+            if (guess > number)
             {
-                Console.Write("Lower");
+                Console.WriteLine("Lower");
             }
-            else if (guess == magicN)
+            else if (guess < number)
             {
-                Console.Write("You guessed it!");
-            }
-            else
-            {
-                Console.Write("Higher");
-            }
+                Console.WriteLine("Higher");
+            }  
         }
-        
+
+        Console.Write("You guessed it!");
 
         
     }
