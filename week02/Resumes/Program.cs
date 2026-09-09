1ee8cae87c6af89baa1662df689cc0c86768fe56
katmich25 <mich_elin@live.com.mx>
2026-09-09 09:27:57 -0600

    Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -21,6 +21,7 @@ class Program
 
         Resume myResume = new Resume();
         myResume._name = "Allison Rose";
+        
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
         myResume.DisplayResume();
